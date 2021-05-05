d495806de371816f446519d2ebb16bb6cd6a7003
Vinayak Pawar <vinayakapawar@gmail.com>
2021-05-05 16:21:24 +0530

    OPSMN-5590: Removed unwanted whitespaces...

--- WEB-INF/src/com/krishagni/catissueplus/core/common/barcodes/BarcodeGenerator.java
+++ WEB-INF/src/com/krishagni/catissueplus/core/common/barcodes/BarcodeGenerator.java
@@ -3,9 +3,7 @@ package com.krishagni.catissueplus.core.common.barcodes;
 import java.awt.image.BufferedImage;
 
 public interface BarcodeGenerator {
-
 	String base64Png(String format, String content, int width, int height);
-
 
 	BufferedImage encode(String format, String content, int width, int height);
 }
